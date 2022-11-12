53f5ae2b85f6bcd62f34ca77f82b6dee98dd50ae
Shawn McSparron <shawnmc_28@hotmail.com>
2022-11-12 11:59:14 +1100

    added validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates :content, :rating, presence: true
+  validates :rating, numericality: { in: 0..5, only_integer: true }
+
 end
--- db/migrate/20221111235609_create_restaurants.rb
+++ db/migrate/20221111235609_create_restaurants.rb
@@ -1,4 +1,5 @@
 class CreateRestaurants < ActiveRecord::Migration[7.0]
+
   def change
     create_table :restaurants do |t|
       t.string :name
