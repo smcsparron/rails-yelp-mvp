3f6b8c11007c8f5fc108386fd7c62c4894b73e56
Shawn McSparron <shawnmc_28@hotmail.com>
2022-11-12 12:53:19 +1100

    added seed file

--- Gemfile
+++ Gemfile
@@ -44,6 +44,8 @@ gem "bootsnap", require: false
 
 gem "simple_form", github: "heartcombo/simple_form"
 
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'main'
+
 
 # Use Sass to process CSS
 # gem "sassc-rails"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning Database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+20.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  puts "Restaurant with id: #{restaurant.id} name: #{restaurant.name} has been created"
+end
+
+puts 'Finished'
